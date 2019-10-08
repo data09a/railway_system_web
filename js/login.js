2316557ae8d031832ce2c7d8b9db80b718fbf40b
Annie <tingwu@TINGs-MacBook-Pro.local>
2019-10-08 06:16:44 -0400

    update

---
+++ js/login.js
@@ -0,0 +1,36 @@
+// alert(1);
+
+var saveUser = window.localStorage.getItem('acc');
+var savePass = window.localStorage.getItem('pass');
+console.log(saveUser);
+
+
+var loginButton = document.getElementById('login-btn');
+
+loginButton.onclick = function(){
+	// alert(1);
+
+	var loginUser = document.getElementById('account-name');
+	var loginPassword = document.getElementById('user-password');
+
+	var inputUser = loginUser.value;
+	var inputPassword = loginPassword.value;
+
+	console.log(inputUser);
+	console.log(inputPassword);
+
+
+	var saveObj = window.localStorage.getItem('userObj');
+	var saveJSONObj =JSON.parse(saveObj);
+	console.log(saveJSONObj);
+
+
+	if(inputUser==saveJSONObj.user&&inputPassword==saveJSONObj.pwd){
+		alert('It is correct');
+	}else{
+		alert('username or password is incorrect!');
+	}
+
+}
+
+
---
+++ js/register.js
@@ -0,0 +1,36 @@
+var registerBtn = document.getElementById('nextBtn');
+registerBtn.onclick	= function(){
+	// alert(1);
+	var userBox = document.getElementById('userName');
+	var passBox = document.getElementById('passWord');
+
+
+	var getUser = userBox.value;
+	var getPass = passBox.value;
+
+ //    // permanent storage
+	// window.localStorage.setItem('acc',getUser);
+	// window.localStorage.setItem('pass', getPass);
+
+     //temporary storage
+	// window.sessionStorage.setItem('acc', getUser);
+	// window.sessionStorage.setItem('pass', getPass);
+
+	//object
+
+	var obj ={
+		user: getUser,
+		pwd: getPass
+	};
+
+
+	// using json change object to. json format string
+
+	var JSONObj = JSON.stringify(obj);
+	console.log(JSONObj);
+    window.localStorage.setItem('userObj', JSONObj);
+    alert('You are registered!');
+
+	window.location.href ="login.html"
+
+}
