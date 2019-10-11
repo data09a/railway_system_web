d387e4de6fd0ad5e4bf91faee7745bfbf6e8f272
Annie <tingwu@TINGs-MacBook-Pro.local>
2019-10-11 12:29:48 -0400

    update

--- js/register.js
+++ js/register.js
@@ -1,3 +1,27 @@
+	var oText = document.getElementById('userName');
+		// oText.onkeydown = function(){
+  //            console.log('I enter a key');
+  //        }
+
+         oText.onkeyup = function(){
+         	var inputUser =oText.value;
+         	var reg =/^[0-9]{4}[A-z]{4}$/;
+         	if (inputUser.match(reg) == null){
+         		console.log('It does not match the requirements');
+         		oText.nextElementSibling.innerHTML ='Please use 4 number&4 letter';
+         		oText.nextElementSibling.style.color ="red";
+
+         	}else{
+         		console.log('the input is correct');    
+         		oText.nextElementSibling.innerHTML ='√';
+         		oText.nextElementSibling.style.color ='green';    		
+
+         	}
+         }
+
+
+
+
 var registerBtn = document.getElementById('nextBtn');
 registerBtn.onclick	= function(){
 	// alert(1);
@@ -18,15 +42,16 @@ registerBtn.onclick	= function(){
 
 	//  make it object and put it in an array
 
+
 	var obj ={
 		user:getUser,
 		pwd:getPass
 	};
 
-	if(window.localStorage.getItem('userObj') == null){
+	if(window.localStorage.getItem('userObj_020a') == null){
 		var inputArr =[];
 	}else{
-		var inputArr =JSON.parse(window.localStorage.getItem('userObj'));
+		var inputArr =JSON.parse(window.localStorage.getItem('userObj_020a'));
     }
 
     var userExists =false;
@@ -42,10 +67,13 @@ registerBtn.onclick	= function(){
     	inputArr.push(obj);
     	var JSONObj = JSON.stringify(inputArr);
 		console.log(JSONObj);
-    	window.localStorage.setItem('userObj', JSONObj);
+    	window.localStorage.setItem('userObj_020a', JSONObj);
     	alert('You are registered!');
     	window.location.href ="login.html";
 
     }
 }
-	
+
+
+
+// 	
